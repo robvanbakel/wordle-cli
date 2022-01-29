73cdf3959aa694b297029fbe3f61037df1aeb708
Rob van Bakel <robbakel@gmail.com>
2022-01-30 00:27:35 +0100

    Refactor showWord

--- bin/showWord.js
+++ bin/showWord.js
@@ -1,29 +1,24 @@
+const formattingCodes = {
+  green: [30, 42],
+  yellow: [30, 43],
+  none: [1, 37, 40],
+  reset: [0],
+}
+
+const format = (color = 'reset') => `\x1b[${formattingCodes[color].join(';')}m`
+const endTile = () => format() + ' '
+const insertSpaces = (num) => new Array(num + 1).join(' ')
+
 const showWord = (result) => {
   let topBottom = ''
   let middle = ''
 
-  result.forEach((item) => {
-    switch (item.color) {
-      case 'green':
-        topBottom += `\x1b[30;42m       \x1b[0m `
-        middle += `\x1b[30;42m   ${item.letter}   \x1b[0m `
-        break
-      case 'yellow':
-        topBottom += `\x1b[43;30m       \x1b[0m `
-        middle += `\x1b[43;30m   ${item.letter}   \x1b[0m `
-        break
-      default:
-        topBottom += `\x1b[1;30;37m       \x1b[0m `
-        middle += `\x1b[1;30;37m   ${item.letter}   \x1b[0m `
-        break
-    }
+  result.forEach(({ color, letter }) => {
+    topBottom += format(color) + insertSpaces(7) + endTile()
+    middle += format(color) + insertSpaces(3) + letter + insertSpaces(3) + endTile()
   })
 
-  console.log('\n')
-  console.log(topBottom)
-  console.log(middle)
-  console.log(topBottom)
-  console.log('\n')
+  console.log(`\n\n${topBottom}\n${middle}\n${topBottom}\n\n`)
 }
 
 module.exports = showWord
