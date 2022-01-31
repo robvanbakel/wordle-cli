580041b9bf68da47192e98fde0cf1eff297a4515
Rob van Bakel <robbakel@gmail.com>
2022-01-31 18:50:42 +0100

    Use UTC dates

--- bin/functions.js
+++ bin/functions.js
@@ -6,13 +6,14 @@ const path = require('path')
 let wordsBuffer = fs.readFileSync(path.join(__dirname, 'words.json'))
 let words = JSON.parse(wordsBuffer)
 
-const get = (date = new Date(), random) => {
+const get = (date = Date.UTC(new Date().getFullYear(), new Date().getMonth(), new Date().getDate()), random) => {
+  
   if (random) {
     return words[Math.floor(Math.random() * words.length)].toUpperCase().split('')
   }
 
   // Get word for specified date
-  const diff = new Date(date) - new Date(2021, 5, 19)
+  const diff = new Date(date) - new Date(Date.UTC(2021, 5, 19))
   const index = Math.floor(diff / 864e5)
   return words[index].toUpperCase().split('')
 }
